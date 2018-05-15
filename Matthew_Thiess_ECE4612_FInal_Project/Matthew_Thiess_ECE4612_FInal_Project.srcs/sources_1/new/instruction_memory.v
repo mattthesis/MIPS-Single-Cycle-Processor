9eeab3530e736594fd03d8364546260756954150
mattthesis <matthewhthiess@gmail.com>
2018-05-15 14:43:27 -0400

    PC and IM
    added program to IMEM module, created synchronous PC

--- Matthew_Thiess_ECE4612_FInal_Project/Matthew_Thiess_ECE4612_FInal_Project.srcs/sources_1/new/instruction_memory.v
+++ Matthew_Thiess_ECE4612_FInal_Project/Matthew_Thiess_ECE4612_FInal_Project.srcs/sources_1/new/instruction_memory.v
@@ -7,17 +7,35 @@ module instruction_memory(input wire clk, input wire [31:0] pc, output reg [31:0
 
 reg [31:0] instruction_mem [0:255];
 
-//load program into memory
-//initial begin
-//    $readmemb("program.mem", instruction_mem);
-//end
-
-//assign instruction = instruction_mem[addr[9:2]];
-
-always@(posedge clk) begin
-
-
-
-
+//initialize instruction memory with program
+initial begin
+    instruction_mem[0]=32'h8C090004; //lw $t1, 0x4($zero)
+    instruction_mem[4]=32'h8C0A0010; //lw $t2, 0x10($zero)
+    instruction_mem[8]=32'h8C0C0014; //lw $t4, 0x14($zero)
+    instruction_mem[12]=32'h8C0D0018; //lw $t5, 0x18($zero)
+    instruction_mem[16]=32'h8C0E001C; //lw $t6, 0x1C($zero)
+    instruction_mem[20]=32'h8C0F0020; //lw $t7, 0x20($zero)
+    instruction_mem[24]=32'h0149582A; //slt $t3, $t2, $t1
+    instruction_mem[28]=32'h11600004; //beq $t3, $zero, less (offset=0x4)
+    
+    instruction_mem[32]=32'h018F4018; //mul $t0, $t4, $t7
+    instruction_mem[36]=32'h010E4020; //add $t0, $t0, $t6
+    instruction_mem[40]=32'h010D401A; //div $t0, $t0, $t5
+    instruction_mem[44]=32'h0800000F; //j save (offset=0xF)
+    
+    //less
+    instruction_mem[48]=32'h018D4018; //mul $t0, $t4, $t5
+    instruction_mem[52]=32'h010E4022; //sub $t0, $t0, $t6
+    instruction_mem[56]=32'h010F4022; //sub $t0, $t0, $t7
+    
+    //save
+    instruction_mem[60]=32'hAC080020; //sw $t0, 0x20($zero)
 end
+
+//update the selected instruction each clock cycle
+always@(posedge clk) begin
+    instruction = instruction_mem[pc]; //program counter gives address of desired instruction
+end
+
+
 endmodule
--- Matthew_Thiess_ECE4612_FInal_Project/Matthew_Thiess_ECE4612_FInal_Project.srcs/sources_1/new/program_counter.v
+++ Matthew_Thiess_ECE4612_FInal_Project/Matthew_Thiess_ECE4612_FInal_Project.srcs/sources_1/new/program_counter.v
@@ -1,7 +1,15 @@
 `timescale 1ns / 1ps
 
-module program_counter( );
+module program_counter(input wire clk, input wire reset, input wire [31:0] next_pc, output reg [31:0] pc);
 
+//initialize the program counter to 0
+initial begin
+    pc = 32'b0;
+end
 
-  
+//update the program counter on each rising clock edge
+always@(posedge clk) begin
+    if(reset==1'b1) pc = 32'b0;
+    else pc = next_pc;
+end
 endmodule
