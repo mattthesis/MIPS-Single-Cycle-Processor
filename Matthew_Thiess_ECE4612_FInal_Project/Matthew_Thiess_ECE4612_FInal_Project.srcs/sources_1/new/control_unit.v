cdb75f830081461b4060f6dc40b634d50c0836c8
mattthesis <matthewhthiess@gmail.com>
2018-05-15 00:26:37 -0400

    declared I/O for control module

--- Matthew_Thiess_ECE4612_FInal_Project/Matthew_Thiess_ECE4612_FInal_Project.srcs/sources_1/new/control_unit.v
+++ Matthew_Thiess_ECE4612_FInal_Project/Matthew_Thiess_ECE4612_FInal_Project.srcs/sources_1/new/control_unit.v
@@ -1,26 +1,8 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/14/2018 11:38:25 PM
-// Design Name: 
-// Module Name: control_unit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module control_unit(
-
-    );
+module control_unit(input wire [5:0] instruction, output reg reg_dst, output reg branch, output reg mem_read, output reg mem_to_reg, 
+                    output reg [1:0] alu_op, output reg mem_write, output reg alu_src, output reg reg_write);
+                    
+                    
+                    
 endmodule
